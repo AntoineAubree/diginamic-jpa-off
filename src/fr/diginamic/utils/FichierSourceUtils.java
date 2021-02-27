29dc1d3ebb93342e81df2cf406f9f3646452152b
DESKTOP-J4CVPG5\Antoine <antoine.aubree@protonmail.com>
2021-02-27 18:18:00 +0100

    new_version

--- src/fr/diginamic/utils/FichierSourceUtils.java
+++ src/fr/diginamic/utils/FichierSourceUtils.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 
@@ -16,16 +15,14 @@ import fr.diginamic.entites.Produit;
 
 public class FichierSourceUtils {
 
-	public static void traiterFichierSource(String chemin, Set<Categorie> categories, Set<Marque> marques,
-			Set<Ingredient> ingredients, Set<Additif> additifs, List<Produit> produits) throws IOException {
+	public static void traiterFichierSource(String chemin) throws IOException {
 		List<String> lignes = recupererLignes(chemin);
 		for (String ligne : lignes) {
-			traiterLigne(categories, marques, ingredients, additifs, produits, ligne);
+			traiterLigne(ligne);
 		}
 	}
 
-	private static void traiterLigne(Set<Categorie> categories, Set<Marque> marques, Set<Ingredient> ingredients,
-			Set<Additif> additifs, List<Produit> produits, String ligne) {
+	private static void traiterLigne(String ligne) {
 
 		String[] decoupage = ligne.split("\\|");
 		String nomCategorie = decoupage[0].toLowerCase();
@@ -41,81 +38,29 @@ public class FichierSourceUtils {
 		String[] decoupageIngredients = decoupage[4].split(",|;");
 		String[] decoupageAdditifs = decoupage[29].split(",|;");
 
-		ajouterCategorieAListeCategories(categories, nomCategorie);
-		ajouterMarqueAListeMarques(marques, nomMarque);
-		ajouterIngredientAListeIngredients(ingredients, decoupageIngredients);
-		ajouterAdditifAListeAdditifs(additifs, decoupageAdditifs);
-		Produit nouveauProduit = ajouterProduitAListeProduits(produits, nomProduit, nutritionGradeFr, energie100g,
+		Produit nouveauProduit = associerAttributsPrimairesAuProduit(nomProduit, nutritionGradeFr, energie100g,
 				graisse100g, sucres100g, fibres100g, proteines100g, sel100g);
-
-		associerCategorieAuProduit(categories, nomCategorie, nouveauProduit);
-		associerMarqueAuProduit(marques, nomMarque, nouveauProduit);
-		associerIngredientsAuProduit(ingredients, decoupageIngredients, nouveauProduit);
-		associerAdditifsAuProduit(additifs, decoupageAdditifs, nouveauProduit);
+		nouveauProduit.setCategorie(new Categorie(nomCategorie));
+		nouveauProduit.setMarque(new Marque(nomMarque));
+		associerIngredientsAuProduit(decoupageIngredients, nouveauProduit);
+		associerAdditifsAuProduit(decoupageAdditifs, nouveauProduit);
+		
+		
+		
 	}
 
-	private static void associerAdditifsAuProduit(Set<Additif> additifs, String[] decoupageAdditifs,
-			Produit nouveauProduit) {
-		for (Additif additif : additifs) {
-			for (String nomAdditif : decoupageAdditifs) {
-				if (additif.getNom().equals(nomAdditif)) {
-					nouveauProduit.addAdditif(additif);
-					break;
-				}
-			}
-		}
+	private static List<String> recupererLignes(String chemin) throws IOException {
+		File file = new File(chemin);
+		List<String> lignes = new ArrayList<>();
+		lignes = FileUtils.readLines(file, "UTF-8");
+		lignes.remove(0);
+		return lignes;
 	}
 
-	private static void associerIngredientsAuProduit(Set<Ingredient> ingredients, String[] decoupageIngredients,
-			Produit nouveauProduit) {
-		for (Ingredient ingredient : ingredients) {
-			for (String nomIngredient : decoupageIngredients) {
-				if (ingredient.getNom().equals(nomIngredient)) {
-					nouveauProduit.addIngredient(ingredient);
-					break;
-				}
-			}
-		}
-	}
-
-	private static void associerMarqueAuProduit(Set<Marque> marques, String nomMarque, Produit nouveauProduit) {
-		for (Marque marque : marques) {
-			if (nomMarque.equals(marque.getNom())) {
-				nouveauProduit.setMarque(marque);
-				break;
-			}
-		}
-	}
-
-	private static void associerCategorieAuProduit(Set<Categorie> categories, String nomCategorie,
-			Produit nouveauProduit) {
-		for (Categorie categorie : categories) {
-			if (nomCategorie.equals(categorie.getNom())) {
-				nouveauProduit.setCategorie(categorie);
-				break;
-			}
-		}
-	}
-
-	private static void ajouterAdditifAListeAdditifs(Set<Additif> additifs, String[] decoupageAdditifs) {
-		for (String nomAdditif : decoupageAdditifs) {
-			Additif nouvelAdditif = new Additif(StringUtils.nettoyerString(nomAdditif));
-			additifs.add(nouvelAdditif);
-		}
-	}
-
-	private static void ajouterIngredientAListeIngredients(Set<Ingredient> ingredients, String[] decoupageIngredients) {
-		for (String nomIngredient : decoupageIngredients) {
-			Ingredient nouvelIngredient = new Ingredient(StringUtils.nettoyerString(nomIngredient));
-			ingredients.add(nouvelIngredient);
-		}
-	}
-
-	private static Produit ajouterProduitAListeProduits(List<Produit> produits, String nomProduit,
-			String nutritionGradeFr, float energie100g, float graisse100g, float sucres100g, float fibres100g,
-			float proteines100g, float sel100g) {
+	private static Produit associerAttributsPrimairesAuProduit(String nomProduit, String nutritionGradeFr,
+			float energie100g, float graisse100g, float sucres100g, float fibres100g, float proteines100g,
+			float sel100g) {
 		Produit nouveauProduit = new Produit(nomProduit);
-		produits.add(nouveauProduit);
 		nouveauProduit.setNutritionGradeFr(nutritionGradeFr);
 		nouveauProduit.setEnergie100g(energie100g);
 		nouveauProduit.setGraisse100g(graisse100g);
@@ -126,22 +71,16 @@ public class FichierSourceUtils {
 		return nouveauProduit;
 	}
 
-	private static void ajouterMarqueAListeMarques(Set<Marque> marques, String nomMarque) {
-		Marque nouvelleMarque = new Marque(nomMarque);
-		marques.add(nouvelleMarque);
+	private static void associerIngredientsAuProduit(String[] decoupageIngredients, Produit nouveauProduit) {
+		for (String nomIngredient : decoupageIngredients) {
+			nouveauProduit.addIngredient(new Ingredient(StringUtils.nettoyerString(nomIngredient)));
+		}
 	}
 
-	private static void ajouterCategorieAListeCategories(Set<Categorie> categories, String nomCategorie) {
-		Categorie nouvelleCategorie = new Categorie(nomCategorie);
-		categories.add(nouvelleCategorie);
-	}
-
-	private static List<String> recupererLignes(String chemin) throws IOException {
-		File file = new File(chemin);
-		List<String> lignes = new ArrayList<>();
-		lignes = FileUtils.readLines(file, "UTF-8");
-		lignes.remove(0);
-		return lignes;
+	private static void associerAdditifsAuProduit(String[] decoupageAdditifs, Produit nouveauProduit) {
+		for (String nomAdditif : decoupageAdditifs) {
+			nouveauProduit.addAdditif(new Additif(StringUtils.nettoyerString(nomAdditif)));
+		}
 	}
 
 }
